531f8cf4b38f08178672eefa718899662e48902a
RieL <choyj1222@gmail.com>
2020-04-19 19:03:20 +0900

    Updated back button

--- app/src/main/java/com/example/caps_android/MainActivity.java
+++ app/src/main/java/com/example/caps_android/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.caps_android;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -26,5 +29,29 @@ public class MainActivity extends AppCompatActivity {
         webView.setWebViewClient(new WebViewClient(){});
 
         webView.loadUrl(webUrlLocal);
+    }
+    private  final long FINISH_INTERNAL_TIME = 2000;
+    private  long backPressedTime = 0;
+
+    @Override
+    public void onBackPressed() { //webView 뒤로가기버튼
+
+        long tempTime = System.currentTimeMillis();
+        long intervalTime = tempTime - backPressedTime;
+
+        if(webView.canGoBack()){
+            if(webView.getUrl().equals("https://caps.dongguk.edu")){
+                if (0 <= intervalTime && FINISH_INTERNAL_TIME >= intervalTime) finish();
+
+                else {
+                    backPressedTime = tempTime;
+                    Toast.makeText(getApplicationContext(), "한번 더 누르면 앱이 종료됩니다.", Toast.LENGTH_SHORT).show();
+                }
+            }
+            else webView.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
     }
 }
